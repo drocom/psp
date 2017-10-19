a4ac319d49aebbf5e92eb6923ef3d9df5944dc0a
GUILLE <guillermodiazhdez@yahoo.es>
2017-10-19 16:20:15 +0200

    Initial commit

---
+++ aparcamiento/src/parking.java
@@ -0,0 +1,5 @@
+/**
+ * Created by GUILLE on 19/10/17.
+ */
+public class parking {
+}
---
+++ aparcamiento/src/principal.java
@@ -0,0 +1,10 @@
+/**
+ * Created by GUILLE on 19/10/17.
+ */
+public class principal {
+    public static void main(String[] args) {
+
+        System.out.println("hola");
+
+    }
+}
